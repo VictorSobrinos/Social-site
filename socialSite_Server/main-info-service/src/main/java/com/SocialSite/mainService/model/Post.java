b51f1955390b0ce6da04f3918e79e110e75e0ea8
Víctor <victorsobrinos@gmail.com>
2023-02-26 20:54:47 +0100

    Post controller added and checked

--- socialSite_Server/main-info-service/src/main/java/com/SocialSite/mainService/model/Post.java
+++ socialSite_Server/main-info-service/src/main/java/com/SocialSite/mainService/model/Post.java
@@ -6,12 +6,17 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer Id;
     private String imgUrl;
     private String comment;
